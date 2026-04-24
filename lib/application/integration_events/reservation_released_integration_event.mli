40910df8a57f1fc9003918ed0d5c184fd65e733d
Ivan Zakrevsky <ivzak@yandex.ru>
2026-04-24 05:40:28 +0300

    Add integration_events

---
+++ lib/application/integration_events/reservation_released_integration_event.mli
@@ -0,0 +1,11 @@
+(** Outbound projection of {!Engine.Portfolio.reservation_released}. *)
+
+type t = {
+  reservation_id : int;
+  side : string;
+  instrument : Queries.Instrument_view_model.t;
+} [@@deriving yojson]
+
+type domain = Engine.Portfolio.reservation_released
+
+val of_domain : domain -> t
